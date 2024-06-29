725ab0d5459e1ee14bfada9f7c3069674b1b1b5e
ahmedragab <ar1755@fayoum.edu.eg>
2024-06-30 01:32:55 +0300

    adding fonts and test it

--- lib/app/snap_shope.dart
+++ lib/app/snap_shope.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:newstore/app/contictivity_controller.dart';
 import 'package:newstore/app/env.varables.dart';
 import 'package:newstore/core/common/screen/no_network_screen.dart';
+import 'package:newstore/core/styles/fonts/font_family_helper.dart';
+import 'package:newstore/core/styles/fonts/font_weight_helper.dart';
 
 class SnapShope extends StatelessWidget {
   const SnapShope({super.key});
@@ -14,9 +17,10 @@ class SnapShope extends StatelessWidget {
       builder: (context, value, child) {
         if (value) {
           return ScreenUtilInit(
+            designSize: const Size(360, 690),
             minTextAdapt: true,
-            designSize: const Size(380, 812),
-            child: MaterialApp(
+            splitScreenMode: true,
+            builder: (context, child) => MaterialApp(
               debugShowCheckedModeBanner: EnvVarables.ins.type,
               builder: (context, widget) {
                 return Scaffold(
@@ -28,7 +32,41 @@ class SnapShope extends StatelessWidget {
                   ),
                 );
               },
-              home: const Scaffold(),
+              home: Scaffold(
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'hello',
+                        style: TextStyle(
+                            fontSize: 20.sp,
+                            fontFamily: FontFamilyHelper.poppinsEnglish,
+                            fontWeight: FontWeightHelper.regular),
+                      ),
+                      Text(
+                        'مرحبا',
+                        style: TextStyle(
+                            fontSize: 20.sp,
+                            fontFamily: FontFamilyHelper.cairoArabic,
+                            fontWeight: FontWeightHelper.regular),
+                      ),
+                      Text(
+                        'hello',
+                        style: TextStyle(
+                            fontSize: 20.sp,
+                            fontWeight: FontWeightHelper.regular),
+                      ),
+                      Text(
+                        'مرحبا',
+                        style: TextStyle(
+                            fontSize: 20.sp,
+                            fontWeight: FontWeightHelper.regular),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
           );
         } else {
---
+++ lib/core/styles/fonts/font_weight_helper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FontWeightHelper {
+  static FontWeight regular = FontWeight.w400;
+  static FontWeight normal = FontWeight.w500;
+  static FontWeight bold = FontWeight.w700;
+  static FontWeight semiBold = FontWeight.w600;
+  static FontWeight extraBold = FontWeight.w800;
+}
