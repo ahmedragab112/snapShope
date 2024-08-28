7937c449f2b926373e785074558e26889ef2177f
ahmedragab <ar1755@fayoum.edu.eg>
2024-08-29 00:45:25 +0300

    adding signup ui

--- lib/features/auth/presentation/view/screens/sign_up.dart
+++ lib/features/auth/presentation/view/screens/sign_up.dart
@@ -1,11 +1,13 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:newstore/core/extension/extension.dart';
 import 'package:newstore/config/router/app_routes.dart';
+import 'package:newstore/core/extension/extension.dart';
 import 'package:newstore/core/styles/style/app_text_style.dart';
 import 'package:newstore/features/auth/presentation/view/widget/custom_button.dart';
+import 'package:newstore/features/auth/presentation/view/widget/sign_up/signup_image.dart';
 
 import '../../../../../app/manager/appcontroller_cubit.dart';
 import '../../../../../core/common/widget/custom_text_formfiled.dart';
@@ -68,7 +70,7 @@ class SignUp extends StatelessWidget {
                 30.verticalSpace,
                 SlideInDown(
                   child: Text(
-                    context.getText.login,
+                    context.getText.signUp,
                     style: context.getTheme.bodyLarge!.copyWith(
                       color: context.getColorTheme.textColor,
                     ),
@@ -79,7 +81,7 @@ class SignUp extends StatelessWidget {
                   animate: true,
                   duration: const Duration(milliseconds: 1000),
                   child: Text(
-                    context.getText.getStarted,
+                    context.getText.addYourInformation,
                     style: context.getTheme.bodyMedium!.copyWith(
                       color: context.getColorTheme.textColor,
                     ),
@@ -87,7 +89,19 @@ class SignUp extends StatelessWidget {
                   ),
                 ),
                 30.verticalSpace,
+                const SignupImage(),
+                30.verticalSpace,
                 FadeInLeft(
+                  child: CustomTextFormFild(
+                    controller: TextEditingController(),
+                    hintText: context.getText.fullName,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 8.w, vertical: 12.h),
+                    leadingIcon: const Icon(Icons.person),
+                  ),
+                ),
+                30.verticalSpace,
+                FadeInRight(
                   child: CustomTextFormFild(
                     controller: TextEditingController(),
                     hintText: context.getText.email,
@@ -128,7 +142,7 @@ class SignUp extends StatelessWidget {
                     width: double.infinity,
                     onTap: () {},
                     child: Text(
-                      context.getText.login,
+                      context.getText.signUp,
                       textAlign: TextAlign.center,
                       style: context.getTheme.bodyMedium!.copyWith(
                         color: context.getColorTheme.textColor,
@@ -139,11 +153,14 @@ class SignUp extends StatelessWidget {
                   ),
                 ),
                 30.verticalSpace,
-                Text(context.getText.createAccount,
-                    style: AppTextSyle.font12Black.copyWith(
-                        color: context.getColorTheme.bluePinkLight,
-                        fontSize: 20.sp,
-                        fontWeight: FontWeight.bold)),
+                GestureDetector(
+                  onTap: () => context.pushNamed(AppRoutes.login),
+                  child: Text(context.getText.alreadyHaveAccount,
+                      style: AppTextSyle.font12Black.copyWith(
+                          color: context.getColorTheme.bluePinkLight,
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.bold)),
+                ),
               ],
             ),
           ),
